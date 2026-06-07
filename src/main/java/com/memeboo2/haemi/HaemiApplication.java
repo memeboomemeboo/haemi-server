7993b27ddeaf458a320a4398a482c8e13ce199a8
황정빈 <hjbin1211@gmail.com>
2026-06-08 08:40:11 +0900

    initial commit

---
+++ src/main/java/com/memeboo2/haemi/HaemiApplication.java
@@ -0,0 +1,13 @@
+package com.memeboo2.haemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaemiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memeboo2/haemi/HaemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memeboo2.haemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
